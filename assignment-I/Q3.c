b05a58e2f71b1df01d14cb29b59280117312d382
kabuki07 <90715530+kabuki07@users.noreply.github.com>
2021-10-10 20:41:59 +0530

    Update Q3.c

--- assignment-I/Q3.c
+++ assignment-I/Q3.c
@@ -2,22 +2,17 @@
 #include<stdlib.h>
 
 int main(){
-
     int n, *arr;
     printf("Size of array: ");
     scanf("%d",&n);
-
     arr = (int*)malloc(n*sizeof(int));
-
     printf("Elements of the array: ");
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-
     int a, b;
     printf("Enter the number of rows and columns respectively: ");
     scanf("%d %d", &a, &b);
-
     if(a*b != n){
         printf("2D matrix can't be formed");
     }
@@ -30,16 +25,12 @@ int main(){
                 c++;
             }
         }
-
         for (int i = 0; i < a; i++){
             for (int j = 0; j < b; j++){
                 printf("%d\t",arr2[i][j]);
             }
             printf("\n");
         }
-
     }
-
     return 0;
-
 }
