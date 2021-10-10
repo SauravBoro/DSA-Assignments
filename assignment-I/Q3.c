2ffeb22e5f85cf314d8fe7b5e4e1995487a63213
kabuki07 <90715530+kabuki07@users.noreply.github.com>
2021-10-10 20:23:38 +0530

    Update Q3.c

--- assignment-I/Q3.c
+++ assignment-I/Q3.c
@@ -2,44 +2,44 @@
 #include<stdlib.h>
 
 int main(){
-  
-  int n, *arr;
-  printf("Enter the size of array: ");
-  scanf("%d",&n);
 
-  arr = (int*)malloc(n*sizeof(int));
+    int n, *arr;
+    printf("Size of array: ");
+    scanf("%d",&n);
 
-  printf("Enter the elements of the array: ");
-  for(int i=0;i<n;i++){
-    scanf("%d",arr+i);
-  }
+    arr = (int*)malloc(n*sizeof(int));
 
-  int a, b;
-  printf("Enter no. of rows and columns (n x m): ");
-  scanf("%d %d", &a, &b);
-
-  if(a*b != n){
-    printf("2D matrix can't be formed");
-  }
-  else{
-    int counter = 0;
-    int arr2[a][b];
-    for (int i = 0; i < a; i++){
-      for (int j = 0; j < b; j++){
-        arr2[i][j] = arr[counter];
-        counter++; 
-      }
+    printf("Elements of the array: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
 
-    for (int i = 0; i < a; i++){
-      for (int j = 0; j < b; j++){
-        printf("%d    ",arr2[i][j]);        
-      }
-      printf("\n");
+    int a, b;
+    printf("Enter the number of rows and columns respectively: ");
+    scanf("%d %d", &a, &b);
+
+    if(a*b != n){
+        printf("2D matrix can't be formed");
     }
-    
-  }
-  
-  return 0;
+    else{
+        int c = 0;
+        int arr2[a][b];
+        for (int i = 0; i < a; i++){
+            for (int j = 0; j < b; j++){
+                arr2[i][j] = arr[c];
+                c++;
+            }
+        }
+
+        for (int i = 0; i < a; i++){
+            for (int j = 0; j < b; j++){
+                printf("%d\t",arr2[i][j]);
+            }
+            printf("\n");
+        }
+
+    }
+
+    return 0;
 
 }
